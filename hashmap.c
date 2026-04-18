92f03d97dd0b0306f19783d6903ea23bce731433
Your Name <you@example.com>
2026-04-18 19:21:50 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -65,7 +65,7 @@ HashMap * createMap(long capacity) {
 void insertMap(HashMap * map, char * key, void * value) {
     long pos=hash(key, map->capacity) ;
 
-    if (search(map, key)!=NULL) {
+    if (searchMap(map, key)!=NULL) {
         return ;
     }
     
